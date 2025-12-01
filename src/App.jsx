639122bf33696beb336bfdb7f2cdb7c38442ee71
Minwook07 <mith.chett@gmail.com>
2025-12-01 10:06:03 +0700

    add page coming-soon

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,32 @@ import { useState } from 'react';
 import { Setting } from './pages/settings/Setting';
 import { UserContent } from './pages/users/UserContent';
 import { users } from './features/users/utils/user-data';
+import { ComingSoon } from './components/coming-soon-page';
 
 function App() {
     const [activePage, setActivePage] = useState("Dashboard");
+
+    const renderPage = () => {
+        switch (activePage) {
+            case "Dashboard":
+                return <Dashboard />
+            case "Attendance":
+                return <AttendanceContent />
+            case "Users":
+                return <UserContent data={users} />
+            case "Settings":
+                return <Setting />
+            default:
+                return <ComingSoon setActivePage={setActivePage}/>
+        }
+    }
 
     return (
         <div>
             <div className="lg:ml-64 pt-16 lg:pt-0">
                 <Sidebar activePage={activePage} setActivePage={setActivePage} />
                 <div className="p-6">
-                    {activePage === "Dashboard" && <Dashboard />}
-                    {activePage === "Attendance" && <AttendanceContent />}
-                    {activePage === "QR" && <h1 className="text-2xl">QR Content</h1>}
-                    {activePage === "Users" && <UserContent data={users} />}
-                    {activePage === "Settings" && <Setting />}
+                    {renderPage()}
                 </div>
             </div>
         </div>
--- src/components/alerts-panel/index.jsx
+++ src/components/alerts-panel/index.jsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export function AlertsPanel({ icon, message, type }) {
 
     const typeColors = {
-        warning: 'bg-yellow-50 border-yellow-600 text-yellow-800',
-        info: 'bg-blue-200 border-blue-600 text-blue-800',
-        success: 'bg-green-100 border-green-600 text-green-800',
+        warning: 'bg-yellow-100 border-yellow-600 text-yellow-900',
+        info: 'bg-blue-200 border-blue-600 text-blue-900',
+        success: 'bg-green-100 border-green-600 text-green-900',
     }
 
     const bgColor = typeColors[type] || 'bg-gray-500'
