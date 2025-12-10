f7dc2fba1308519e76e89cb9fd66753f1d680aa4
Minwook07 <mith.chett@gmail.com>
2025-12-10 10:09:19 +0700

    add ui login

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,10 @@ import { users } from './features/users/utils/user-data';
 import { ComingSoon } from './components/coming-soon-page';
 import { LogoutModal } from './features/auth/components/LogoutModal'
 import { Footer } from './layouts/Footer';
+import { Login } from './features/auth/components/Login';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [logoutOpen, setLogoutOpen] = useState(false)
     const [activePage, setActivePage] = useState("Dashboard")
     const [sidebarOpen, setSidebarOpen] = useState(true)
@@ -32,28 +34,34 @@ function App() {
     }
 
     return (
-        <div>
-            <Sidebar
-                activePage={activePage}
-                setActivePage={setActivePage}
-                openLogoutModal={() => setLogoutOpen(true)}
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-            />
-            <LogoutModal
-                isOpen={logoutOpen}s
-                onClose={() => setLogoutOpen(false)}
-                onLogout={() => console.log("Logout clicked")}
-                setActivePage={setActivePage}
-            />
-            <div className={`transition-all duration-300 ${sidebarOpen ? 'lg:ml-64' : 'lg:ml-0'}`}>
-                <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} sidebarOpen={sidebarOpen} openLogoutModal={() => setLogoutOpen(true)} />
-                <div className="p-6 mt-20 mb-20">
-                    {renderPage()}
+        <>
+            {!isLoggedIn ? (
+                <Login />
+            ) : (
+                <div>
+                    <Sidebar
+                        activePage={activePage}
+                        setActivePage={setActivePage}
+                        openLogoutModal={() => setLogoutOpen(true)}
+                        sidebarOpen={sidebarOpen}
+                        setSidebarOpen={setSidebarOpen}
+                    />
+                    <LogoutModal
+                        isOpen={logoutOpen} s
+                        onClose={() => setLogoutOpen(false)}
+                        onLogout={() => console.log("Logout clicked")}
+                        setActivePage={setActivePage}
+                    />
+                    <div className={`transition-all duration-300 ${sidebarOpen ? 'lg:ml-64' : 'lg:ml-0'}`}>
+                        <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} sidebarOpen={sidebarOpen} openLogoutModal={() => setLogoutOpen(true)} />
+                        <div className="p-6 mt-20 mb-20">
+                            {renderPage()}
+                        </div>
+                        <Footer toggleSidebar={() => setSidebarOpen(!sidebarOpen)} sidebarOpen={sidebarOpen} />
+                    </div>
                 </div>
-                <Footer toggleSidebar={() => setSidebarOpen(!sidebarOpen)} sidebarOpen={sidebarOpen} />
-            </div>
-        </div>
+            )}
+        </>
     );
 }
 
